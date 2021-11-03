acb9f3413ce448847268753b74c29debef4d97dd
Angelopezmacc <angel.lopez@urosario.edu.co>
2021-11-02 21:17:00 -0500

    probando

---
+++ proyecto (copia).R
@@ -0,0 +1,34 @@
+# Universidad del Rosario
+# Proyecto final
+# Probailidad y Estadística 1
+# 2021 - 2s
+# 
+# Juan Obando
+# Ángel López
+
+#--------------------------------------------------------------------------------------------
+# Se importa el dataset
+library(readxl)
+dataset_engineering_graduate_salary <- read_excel("Documentos/UR 2021-2/Estadística/Proyecto/dataset_engineering_graduate_salary.xlsx")
+View(dataset_engineering_graduate_salary)
+
+#--------------------------------------------------------------------------------------------
+print('prueba')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
